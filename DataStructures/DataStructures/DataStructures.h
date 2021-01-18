e291e4deddd7c89cfb3b85b536654b463db72cc0
sudo.park <sudo.park@kakaocorp.com>
2021-01-19 01:52:54 +0900

    add workspace and projects

---
+++ DataStructures/DataStructures/DataStructures.h
@@ -0,0 +1,18 @@
+//
+//  DataStructures.h
+//  DataStructures
+//
+//  Created by Sudo.park on 2021/01/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataStructures.
+FOUNDATION_EXPORT double DataStructuresVersionNumber;
+
+//! Project version string for DataStructures.
+FOUNDATION_EXPORT const unsigned char DataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructures/PublicHeader.h>
+
+
---
+++ Practice/Practice/Practice.h
@@ -0,0 +1,18 @@
+//
+//  Practice.h
+//  Practice
+//
+//  Created by Sudo.park on 2021/01/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Practice.
+FOUNDATION_EXPORT double PracticeVersionNumber;
+
+//! Project version string for Practice.
+FOUNDATION_EXPORT const unsigned char PracticeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Practice/PublicHeader.h>
+
+
